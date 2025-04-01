dfb6de0ff7bc8d6d903cdf9a489ad96e10d8504f
양희태 <122270969+smyang0220@users.noreply.github.com>
2025-04-02 06:06:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.28 ms, Memory: 59 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,54 +1,47 @@
 import java.util.*;
 
 class Solution {
-    // 여기에 Node 클래스를 내부 클래스로 정의
-    class Node {
-        int x, y;
-        Node(int x, int y) {
+    class position {
+        int x;
+        int y;
+
+        public position(int x, int y) {
             this.x = x;
             this.y = y;
         }
     }
-    
-    // 방향 벡터 정의
-    int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, 1, 0, -1};
-    
+
+    int[] nx = {0, 1, 0, -1};
+    int[] ny = {1, 0, -1, 0};
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-        
-        // BFS를 위한 큐 생성
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(0, 0));
-        
-        // 시작점은 이미 방문했으므로 거리는 1
-        maps[0][0] = 1;
-        
-        while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-            int x = cur.x;
-            int y = cur.y;
-            
-            // 현재 위치가 목적지인 경우
-            if (x == n - 1 && y == m - 1) {
-                return maps[x][y];
-            }
-            
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                
-                // 경계 체크 및 방문하지 않은 길(1)인지 확인
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m || maps[nx][ny] != 1) continue;
-                
-                // 새 위치의 거리 = 현재 위치 거리 + 1
-                maps[nx][ny] = maps[x][y] + 1;
-                queue.offer(new Node(nx, ny));
+        int h = maps.length;
+        int w = maps[0].length;
+
+        Queue<position> q = new LinkedList<>();
+        q.add(new position(0,0));
+
+        boolean[][] visited = new boolean[h][w];
+        visited[0][0] = true;
+
+        position p;
+        while (!q.isEmpty()) {
+            p = q.poll();
+            int x = p.x;
+            int y = p.y;
+
+            for(int i=0; i<4; i++) {
+                int next_x = x + nx[i];
+                int next_y = y + ny[i];
+
+                if (0 <= next_x && next_x < h && 0 <= next_y && next_y < w){
+                    if(visited[next_x][next_y] == false && maps[next_x][next_y] > 0) {
+                        visited[next_x][next_y] = true;
+                        maps[next_x][next_y] = maps[x][y] + 1;
+                        q.add(new position(next_x, next_y));
+                    }
+                }
             }
         }
-        
-        // 목적지에 도달하지 못했을 경우
-        return -1;
+        return maps[h-1][w-1] == 1 ? -1 : maps[h-1][w-1];
     }
 }
