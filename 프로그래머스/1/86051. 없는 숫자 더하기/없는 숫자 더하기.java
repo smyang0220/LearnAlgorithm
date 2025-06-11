3cf877efa3c2428e257cdd8a77140a370a09dc83
양희태 <122270969+smyang0220@users.noreply.github.com>
2025-06-11 23:19:57 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        
+        int answer = 0;
+        
+        boolean[] flag = new boolean[10];
+        
+        for(int num : numbers){
+            flag[num] = true;
+        }
+        
+        for(int i = 0; i <10; i++){
+            if(!flag[i]){
+                answer+=i;
+            }
+        }
+        
+        return answer;
+    }
+}
