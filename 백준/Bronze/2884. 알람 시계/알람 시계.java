0781670fd651c5c0392c0df8c82b9e15f0d4ee1f
양희태 <122270969+smyang0220@users.noreply.github.com>
2025-07-17 20:18:05 +0900

    [Bronze III] Title: 알람 시계, Time: 112 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,22 +1,47 @@
-import java.util.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/2884
+// 알람시계
+// 45분 일찍 알람을 맞추면 된다.
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
+
+        int hour = 0;
+        int minute = 0;
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] time = new int[2];     
-        int index = 0;             
-        while(st.hasMoreTokens()){
-            time[index] = Integer.parseInt(st.nextToken());
-            index++;
-        }
+
+        hour = Integer.parseInt(st.nextToken());
+        minute = Integer.parseInt(st.nextToken());
+
+        setAlram(hour, minute);
         
-        if(time[1] > 44){
-            System.out.printf("%d %d",time[0],time[1]-45);
+    }
+
+    public static void setAlram(int hour, int minute){
+
+        int[] answer = new int[2];
+
+        if(minute<45){
+            answer[1] = minute + 15;
+            hour -= 1;
         }else{
-            System.out.printf("%d %d",(time[0]+23)%24,15+time[1]);
+            answer[1] = minute - 45;
         }
-       
+
+        if(hour < 0 ){
+            answer[0] = 23;
+        }else{
+            answer[0] = hour;
+        }
+
+        System.out.printf("%d %d",answer[0],answer[1]);
     }
 }
