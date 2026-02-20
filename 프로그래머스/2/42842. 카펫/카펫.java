3814878ebd8e602d70f84a27ef5d4614f857d42e
양희태 <122270969+smyang0220@users.noreply.github.com>
2026-02-20 22:48:41 +0900

    [level 2] Title: 카펫, Time: 10.23 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        
+        
+            
+        for(int width = 3; width<= 5000; width++){
+            for(int height = 3; height <=width; height++){
+                int boundary = (width + height - 2) * 2;
+                int center = width * height - boundary; 
+                if(center == yellow && boundary == brown)
+                return new int[] {width, height};
+            }
+        }
+        return null;
+    }
+}
