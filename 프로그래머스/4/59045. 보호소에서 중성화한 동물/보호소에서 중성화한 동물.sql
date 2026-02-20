ea2acc2865fd6f39929c482bd88052ac715aa4f1
양희태 <122270969+smyang0220@users.noreply.github.com>
2026-02-20 14:53:03 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT i.ANIMAL_ID,i.ANIMAL_TYPE,i.NAME
+from ANIMAL_INS i 
+join ANIMAL_OUTS o 
+on i.ANIMAL_ID = o.ANIMAL_ID
+where i.SEX_UPON_INTAKE != o.SEX_UPON_OUTCOME
+order by ANIMAL_ID
